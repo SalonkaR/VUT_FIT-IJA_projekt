adde1f7c5c16652d9182da205ca1204cedd4139d
Sokol9 <jsokolik99@gmail.com>
2021-04-03 14:07:09 +0200

    vytvorenie cordinate class s geter a setter

---
+++ src/main/java/Cordinate.java
@@ -0,0 +1,55 @@
+public class Cordinate {
+    private double x;
+    private double y;
+
+    public Cordinate(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Cordinate{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cordinate)) return false;
+
+        Cordinate cordinate = (Cordinate) o;
+
+        if (Double.compare(cordinate.x, x) != 0) return false;
+        return Double.compare(cordinate.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(x);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(y);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+}
