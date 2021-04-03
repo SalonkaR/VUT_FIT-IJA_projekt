6435a776863f787ddcc673b88a7b583e3cd8a347
Sokol9 <jsokolik99@gmail.com>
2021-04-03 14:20:02 +0200

    vytvorenie Shelf class iba s constructorom, refacrorizacia projekto, pridanie ija.project package

---
+++ src/main/java/ija/project/Main.java
@@ -0,0 +1,12 @@
+package ija.project;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.show();
+    }
+}
