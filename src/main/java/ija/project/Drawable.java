37e2bbba81933147ae02ae6ebded221d96568637
Sokol9 <jsokolik99@gmail.com>
2021-04-03 15:18:12 +0200

    vytvorenie interface Drawable, rozsirenie triedy Shelf na implemntaciu Drawable implementacia getGUI()

---
+++ src/main/java/ija/project/Drawable.java
@@ -0,0 +1,9 @@
+package ija.project;
+
+import javafx.scene.shape.Shape;
+
+import java.util.List;
+
+public interface Drawable {
+    List<Shape> getGUI();
+}
--- src/main/java/ija/project/Shelf.java
+++ src/main/java/ija/project/Shelf.java
@@ -1,11 +1,28 @@
 package ija.project;
 
-public class Shelf {
-    private Cordinate TopLeft;
-    private Cordinate BottomRight;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
-    public Shelf(Cordinate topLeft, Cordinate bottomRight) {
-        TopLeft = topLeft;
-        BottomRight = bottomRight;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shelf implements Drawable {
+    private Cordinate position;
+    private double width = 50;
+    private double height = 50;
+
+    private List<Shape> gui;
+
+    public Shelf(Cordinate position, double width, double height) {
+        this.position = position;
+        this.width = width;
+        this.height = height;
+        gui = new ArrayList<>();
+        gui.add(new Rectangle(position.getX(), position.getY(), width, height));
+    }
+
+    @Override
+    public List<Shape> getGUI() {
+        return gui;
     }
 }
